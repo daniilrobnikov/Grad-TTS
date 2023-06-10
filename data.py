25986807092ef34ac7f3a73943edb0d0b89b93d8
Daniil Robnikov <daniilrobnikov@gmail.com>
2023-06-10 15:37:46 +0300

    Pyworld setup

--- data.py
+++ data.py
@@ -113,7 +113,7 @@ class TextMelBatchCollate(object):
         y_max_length = max([item["y"].shape[-1] for item in batch])
         y_max_length = fix_len_compatibility(y_max_length)
         x_max_length = max([item["x"].shape[-1] for item in batch])
-        y_pitch_max_length = max([item["y_pitch"].shape[-1] for item in batch])
+        y_pitch_max_length = max([item["pitch"].shape[-1] for item in batch])
         n_feats = batch[0]["y"].shape[-2]
 
         y = torch.zeros((B, n_feats, y_max_length), dtype=torch.float32)
