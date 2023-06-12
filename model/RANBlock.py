0cffe0e03391f57cd1731e4ea1f598283c58cacd
Daniil Robnikov <daniilrobnikov@gmail.com>
2023-06-12 18:36:49 +0300

    Update RANBlock

--- model/RANBlock.py
+++ model/RANBlock.py
@@ -32,90 +32,78 @@ class TrunkBranch(nn.Module):
             self.layer.append(ResUnit(dim_out, dim_out))
 
     def forward(self, x):
+        output = x
         for layer in self.layer:
-            x = layer(x)
-        return x
+            output = layer(output)
+        return output
 
 
 class MaskBranch(nn.Module):
     def __init__(self, dim, dim_out, r=1):
         super(MaskBranch, self).__init__()
-
         self.pre_layer = nn.ModuleList()
         self.pre_layer.append(nn.AvgPool2d(3, stride=2, padding=1, count_include_pad=False))
         self.pre_layer.append(ResUnit(dim, dim_out))
         for _ in range(r - 1):
             self.layer.append(ResUnit(dim_out, dim_out))
-
         self.layer = nn.ModuleList()
         self.layer.append(nn.AvgPool2d(3, stride=2, padding=1, count_include_pad=False))
         for _ in range(2 * r):
             self.layer.append(ResUnit(dim_out, dim_out))
-
         self.post_layer = nn.ModuleList()
         for _ in range(r):
             self.post_layer.append(ResUnit(dim_out, dim_out))
 
         self.conv1 = nn.Conv2d(dim_out, dim_out, 1)
         self.conv2 = nn.Conv2d(dim_out, dim_out, 1)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
+        output = x
         for layer in self.pre_layer:
-            x = layer(x)
-
+            output = layer(output)
         for layer in self.layer:
-            x = layer(x)
-        x = F.interpolate(x, size=x.shape[2:], mode='bilinear', align_corners=True)
-
+            output = layer(output)
+        output = F.interpolate(output, size=x.shape[2:], mode='bilinear', align_corners=True)
         for layer in self.post_layer:
-            x = layer(x)
-        x = F.interpolate(x, size=x.shape[2:], mode='bilinear', align_corners=True)
-        x = self.conv1(x)
-        x = self.conv2(x)
-        return x
+            output = layer(output)
+        output = F.interpolate(output, size=x.shape[2:], mode='bilinear', align_corners=True)
+        output = self.conv1(output)
+        output = self.conv2(output)
+        output = self.softmax(output)
+        return output
 
 
 class AttentionModule(nn.Module):
     def __init__(self, dim, dim_out, p=1, t=2, r=1):
         super(AttentionModule, self).__init__()
-
         self.input_units = nn.ModuleList()
         for _ in range(p):
             self.input_units.append(ResUnit(dim, dim))
-
         self.trunk_branch = TrunkBranch(dim, dim_out, t)
         self.mask_branch = MaskBranch(dim, dim_out, r)
-
         self.output_units = nn.ModuleList()
         for _ in range(p):
             self.output_units.append(ResUnit(dim_out, dim_out))
 
     def forward(self, x, mask):
-        output = x * mask
-        # Input units
         for layer in self.input_units:
             x = layer(x)
 
-        # Trunk branch
         output_trunk = self.trunk_branch(x)
-        # Mask branch
         output_mask = self.mask_branch(x)
-        # Combine both
         output = (1 + output_mask) * output_trunk
 
-        # Output units
         for layer in self.input_units:
-            output = layer(output)
+            x = layer(x)
         return output * mask
 
 
 class RANBlock(nn.Module):
     def __init__(self, dim, dim_out, time_emb_dim):
         super(RANBlock, self).__init__()
-
         self.attention_module = AttentionModule(dim, dim_out)
         self.mlp = torch.nn.Sequential(nn.Mish(), torch.nn.Linear(time_emb_dim, dim_out))
-
         if dim != dim_out:
             self.res_conv = torch.nn.Conv2d(dim, dim_out, 1)
         else:
